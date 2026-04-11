12b6b8354efc5ab1763adaf9a4963ff4343709dc
Nikhil Singh <nikhilrajpu236@gmail.com>
2026-04-11 18:58:35 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+import app from "./app.js";
+import mongoose from "mongoose";
+import "dotenv/config";
+
+const db = process.env.DATABASE.replace(
+  "<DB_PASSWORD>",
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(db)
+  .then(() => {
+    console.log("Databse connected successfully..");
+  })
+  .catch((err) => {
+    console.log("Error 💥:", err);
+  });
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is listen on port: ${port}....`);
+});
