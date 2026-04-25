9078a36129afdeb50a38c6c30107b23b2b4fdbeb
Nikhil Singh <nikhilrajpu236@gmail.com>
2026-04-25 20:11:00 +0530

    preparing for deployment

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ import cors from "cors";
 import { interviewRouter } from "./routes/interviewRouter.js";
 import resumeRouter from "./routes/resumeRouter.js";
 import helmet from "helmet";
+import ExpressMongoSanitize from "express-mongo-sanitize";
+import hpp from "hpp";
+import rateLimit from "express-rate-limit";
+import compression from "compression";
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: "10kb" }));
 app.use(cookieParser());
 
 app.use("/img/users", express.static("public/img/users"));
@@ -21,8 +25,18 @@ const corsOptions = {
   optionsSuccessStatus: 200,
 };
 
+const limiter = rateLimit({
+  max: 10,
+  windowMs: 5 * 60 * 1000,
+  message: "Too many request. Please wait for 5 minutes.",
+});
+
 app.use(cors(corsOptions));
 app.use(helmet());
+app.use(ExpressMongoSanitize());
+app.use(hpp());
+app.use("/api/v1/users/resetPassword/:token", limiter);
+app.use(compression());
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/interviews", interviewRouter);
