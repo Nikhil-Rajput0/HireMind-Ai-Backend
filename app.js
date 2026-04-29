f40b827cd3b889b9c58178f4feddc47533d3d9ee
Nikhil Singh <nikhilrajpu236@gmail.com>
2026-04-29 11:41:46 +0530

    Fixing bug

--- app.js
+++ app.js
@@ -36,12 +36,6 @@ app.use(hpp());
 app.use("/api/v1/users/resetPassword/", limiter);
 app.use(compression());
 
-app.use("/", (req, res) => {
-  res.status(200).json({
-    status: "success",
-    message: "Welcome to hiremind ai backend",
-  });
-});
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/interviews", interviewRouter);
 app.use("/api/v1/resume", resumeRouter);
